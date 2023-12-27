091b6db4f83b65a17b3ad6daf54bdfd42b341fe3
sleepyhazzzel <yangching.emily@gmail.com>
2023-12-28 02:16:41 +0800

    moreInfo button style

--- templates/more_info.js
+++ templates/more_info.js
@@ -159,6 +159,7 @@ export default () => {
             "uri": "http://linecorp.com/"
           },
           "height": "sm",
+          "style": "secondary",
           "offsetBottom": "sm"
         }
       ]
